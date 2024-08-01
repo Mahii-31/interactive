45391d54f8e1bb8c0e30d7a2971243c1df56a6d6
Mahii-31 <rmjmahadevan31@gmail.com>
2024-08-01 11:59:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,32 @@ document.addEventListener('DOMContentLoaded', function() {
         contactForm.reset();
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const modalButtons = document.querySelectorAll('.view-more-btn');
+    const modals = document.querySelectorAll('.modal');
+    const closeButtons = document.querySelectorAll('.close-btn');
+
+    modalButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const modalId = this.getAttribute('data-modal');
+            const modal = document.getElementById(modalId);
+            modal.style.display = "block";
+        });
+    });
+
+    closeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const modal = this.closest('.modal');
+            modal.style.display = "none";
+        });
+    });
+
+    window.addEventListener('click', function(event) {
+        modals.forEach(modal => {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        });
+    });
+});
+
